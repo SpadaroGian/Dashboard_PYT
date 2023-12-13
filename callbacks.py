48f38168701d1929f5adf17605a4cf1f49dacc0d
Gian <146926602+SpadaroGian@users.noreply.github.com>
2023-12-13 11:18:23 +0100

    Feat : Map added , update of graph ( multi countries)

--- callbacks.py
+++ callbacks.py
@@ -1,29 +1,66 @@
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objs as go
 
-def update_graph(df, selected_country):
-    filtered_df = df[df['Country'] == selected_country]
-    fig = px.line(filtered_df, x='Year', y='Population', title=f'Population Trend for {selected_country}')
-    fig.update_xaxes(categoryorder='category ascending')
+def update_graph(df, selected_countries):
+    filtered_df = df[df['Country'].isin(selected_countries)]
+    fig = px.line(filtered_df, x='Year', y='Population', color='Country',
+                  title='Population Trends for Selected Countries')
     return fig
 
-def update_histogram(df, selected_country):
-    filtered_df = df[df['Country'] == selected_country]
-    fig = px.histogram(filtered_df, x='Year', y='Population', title=f'Population Distribution for {selected_country}')
-    fig.update_xaxes(categoryorder='category ascending')
+def update_histogram(df, selected_countries):
+    filtered_df = df[df['Country'].isin(selected_countries)]
+    fig = px.histogram(filtered_df, x='Population', color='Country',
+                       title='Population Distribution for Selected Countries')
+    return fig
+
+def update_map(df, selected_year, selected_countries):
+    filtered_df = df[(df['Country'].isin(selected_countries)) & (df['Year'] == selected_year)]
+    
+    fig = go.Figure()
+
+    for country in selected_countries:
+        country_data = filtered_df[filtered_df['Country'] == country]
+        if not country_data.empty:
+            fig.add_trace(go.Choropleth(
+                locations=[country_data.iloc[0]['Country']],
+                z=[country_data.iloc[0]['Population']],
+                locationmode='country names',
+                colorscale='Viridis',
+                marker_line_color='white',
+                name=country
+            ))
+
+    fig.update_layout(
+        title_text=f'Population by Country for the Selected Year ({selected_year})',
+        geo=dict(
+            showframe=False,
+            showcoastlines=False,
+            projection_type='equirectangular'
+        )
+    )
+
     return fig
 
 def register_callbacks(app, df):
     @app.callback(
         Output('population-graph', 'figure'),
-        [Input('country-dropdown', 'value')]
+        Input('country-dropdown', 'value')
     )
-    def update_graph_callback(selected_country):
-        return update_graph(df, selected_country)
-
+    def update_graph_callback(selected_countries):
+        return update_graph(df, selected_countries)
+    
     @app.callback(
         Output('population-histogram', 'figure'),
-        [Input('country-dropdown', 'value')]
+        Input('country-dropdown', 'value')
     )
-    def update_histogram_callback(selected_country):
-        return update_histogram(df, selected_country)
+    def update_histogram_callback(selected_countries):
+        return update_histogram(df, selected_countries)
+    
+    @app.callback(
+        Output('population-map', 'figure'),
+        Input('year-dropdown', 'value'),
+        Input('country-dropdown', 'value')
+    )
+    def update_map_callback(selected_year, selected_countries):
+        return update_map(df, selected_year, selected_countries)
--- dashboard_layout.py
+++ dashboard_layout.py
@@ -7,12 +7,22 @@ def create_layout(df):
             dcc.Dropdown(
                 id='country-dropdown',
                 options=[{'label': country, 'value': country} for country in df['Country'].unique()],
-                value='Africa Eastern and Southern'
+                value=['Afghanistan'],  # Set default selected countries here
+                multi=True  # Allow multiple selection
             ),
             dcc.Graph(id='population-graph')
-        ], style={'display': 'inline-block', 'width': '70%'}),
+        ], style={'display': 'inline-block', 'width': '50%'}),
         html.Div([
             dcc.Graph(id='population-histogram')
-        ], style={'display': 'inline-block', 'width': '30%'}),
+        ], style={'display': 'inline-block', 'width': '50%'}),
+        html.Div([
+            dcc.Dropdown(
+                id='year-dropdown',
+                options=[{'label': year, 'value': year} for year in df['Year'].unique()],
+                value=df['Year'].min(),  # Set default year here
+                clearable=False
+            ),
+            dcc.Graph(id='population-map')
+        ])
     ])
     return layout
